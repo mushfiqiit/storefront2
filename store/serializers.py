f803d2a42cf1e0840adab54dccf119b4016c3aba
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-03-27 09:05:10 +0600

    Added Rest Api

--- store/serializers.py
+++ store/serializers.py
@@ -1,15 +1,25 @@
-from turtle import title
 from decimal import Decimal
-from unittest.util import _MAX_LENGTH
+from store.models import Product, Collection
 from rest_framework import serializers
 
-from store.models import Product
 
-class ProductSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField(max_length=255)
-    price=serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax')
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
 
-    def calculate_tax(self, product:Product):
+    products_count = serializers.IntegerField()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+   
