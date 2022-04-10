32a8450f5d9b0577ccee11d8d7ab6b5dfb87ebdb
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-04-10 13:04:42 +0600

    Adding Cart

--- store/urls.py
+++ store/urls.py
@@ -1,14 +1,21 @@
 from cgitb import lookup
 from email.mime import base
 from django.urls import path, include
+
+from store.models import Cart
 from . import views
 from rest_framework_nested import routers
 from . import views
 
 router=routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
+router.register('carts', views.CartViewSet)
 
 products_router=routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+carts_router=routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 # URLConf
-urlpatterns = router.urls
+urlpatterns = router.urls+products_router.urls+carts_router.urls
--- store/views.py
+++ store/views.py
@@ -6,19 +6,23 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin
+from store.pagination import DefaultPagination
 from .filters import ProductFilter
-from .models import Collection, Product
-from .serializers import CollectionSerializer, ProductSerializer
+from .models import Collection, Product, Review, Cart, CartItem
+from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerailizer
 
 
 class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
-    filter_backends=[DjangoFilterBackend, SearchFilter]
+    filter_backends=[DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class=ProductFilter
+    pagination_class=DefaultPagination
     search_fields=['title', 'description']
+    ordering_fields=['unit_price', 'last_update']
     
     def get_serializer_context(self):
         return { 'request':self.request }
@@ -28,35 +32,35 @@ class ProductViewSet(ModelViewSet):
         if(product.orderitems.count()>0):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(products_count=Count('products')).all()
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ReviewViewSet(ModelViewSet):
+    serializer_class=ReviewSerailizer
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        return { 'product_id':self.kwargs['product_pk']}
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk):
-    collection = get_object_or_404(
-        Collection.objects.annotate(
-            products_count=Count('products')), pk=pk)
-    if request.method == 'GET':
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CollectionSerializer(collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        if collection.products.count() > 0:
-            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CartViewSet(CreateModelMixin, 
+                RetrieveModelMixin,
+                DestroyModelMixin, 
+                GenericViewSet):
+    queryset=Cart.objects.prefetch_related('items__product').all()
+    serializer_class=CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class=CartItemSerializer
+
+    def get_serializer_context(self):
+        return { 'cart_id':self.kwargs['cart_pk'] }
+
+    def get_queryset(self):
+        return CartItem.objects \
+            .filter(cart_id=self.kwargs['cart_pk']) \
+            .select_related('product')
+
+    def get_serializer_class(self):
+        if self.request.method=='POST':
+            return AddCartItemSerializer
+        return CartItemSerializer
